2e86e794c5b35bda492c86de2a29da64aa7861fd
beWanja <wanjabeauttah@gmail.com>
2022-04-02 02:28:44 -0400

    Made an upgrade on the project: Allows user to navigate to different screens - Profile, Home, and Compose - via a bottom navigation tool bar.

--- app/src/main/java/com/example/parstagram/MainActivity.kt
+++ app/src/main/java/com/example/parstagram/MainActivity.kt
@@ -13,6 +13,12 @@ import android.widget.EditText
 import android.widget.ImageView
 import android.widget.Toast
 import androidx.core.content.FileProvider
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import com.example.parstagram.fragments.ComposeFragment
+import com.example.parstagram.fragments.HomeFragment
+import com.example.parstagram.fragments.ProfileFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.parse.FindCallback
 import com.parse.ParseFile
 import com.parse.ParseQuery
@@ -21,130 +27,45 @@ import java.io.File
 import java.text.ParseException
 import java.util.*
 
-/*
-*User create a post by taking photo
- */
-
-
 class MainActivity : AppCompatActivity() {
-    val CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 1034
-    val photoFileName = "photo.jpg"
-    var photoFile: File? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        findViewById<Button>(R.id.button_postPhoto).setOnClickListener{
-            val description = findViewById<EditText>(R.id.post_description).text.toString()
-            val user = ParseUser.getCurrentUser()
-            if(photoFile != null){
-                submitPost(description,user, photoFile!!)
-            }else{
-                //Can show the user!
-            }
-        }
-        findViewById<Button>(R.id.button_takePhoto).setOnClickListener{
-            onLaunchCamera()
-        }
-        queryPosts()
-    }
+        val fragmentManager: FragmentManager = supportFragmentManager
 
-    fun submitPost(description: String, user: ParseUser, file: File) {
-        val post = Post()
-        post.setDescription(description)
-        post.setUser(user)
-        post.setImage(ParseFile(file))
-        post.saveInBackground{exception ->
-            if(exception != null){
-                Log.e(TAG, "Error while saving post")
-                exception.printStackTrace()
+        findViewById<BottomNavigationView>(R.id.bottom_navigation).setOnItemSelectedListener {
+            item -> //Create a variable instead of calling a generic variable(enhances code readability
 
-                //several todo
-            }else{
-                Log.i(TAG, "Successfully saved post!")
-            }
-        }
-    }
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE) {
-            if (resultCode == RESULT_OK) {
-                // by this point we have the camera photo on disk
-                val takenImage = BitmapFactory.decodeFile(photoFile!!.absolutePath)
-                // RESIZE BITMAP, see section below
-                // Load the taken image into a preview
-                val ivPreview: ImageView = findViewById(R.id.ivPhoto)
-                ivPreview.setImageBitmap(takenImage)
-            } else { // Result was a failure
-                Toast.makeText(this, "Picture wasn't taken!", Toast.LENGTH_SHORT).show()
-            }
-        }
-    }
-    fun onLaunchCamera() {
-        // create Intent to take a picture and return control to the calling application
-        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-        // Create a File reference for future access
-        photoFile = getPhotoFileUri(photoFileName)
+            var fragmentToShow: Fragment? = null
+            when(item.itemId){
 
-        // wrap File object into a content provider
-        // required for API >= 24
-        // See https://guides.codepath.com/android/Sharing-Content-with-Intents#sharing-files-with-api-24-or-higher
-        if (photoFile != null) {
-            val fileProvider: Uri =
-                FileProvider.getUriForFile(this, "com.codepath.fileprovider", photoFile!!)
-            intent.putExtra(MediaStore.EXTRA_OUTPUT, fileProvider)
-
-            // If you call startActivityForResult() using an intent that no app can handle, your app will crash.
-            // So as long as the result is not null, it's safe to use the intent.
-
-            // If you call startActivityForResult() using an intent that no app can handle, your app will crash.
-            // So as long as the result is not null, it's safe to use the intent.
-            if (intent.resolveActivity(packageManager) != null) {
-                // Start the image capture intent to take photo
-                startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE)
-            }
-        }
-    }
-
-    // Returns the File for a photo stored on disk given the fileName
-    fun getPhotoFileUri(fileName: String): File {
-        // Get safe storage directory for photos
-        // Use `getExternalFilesDir` on Context to access package-specific directories.
-        // This way, we don't need to request external read/write runtime permissions.
-        val mediaStorageDir =
-            File(getExternalFilesDir(Environment.DIRECTORY_PICTURES), TAG)
-
-        // Create the storage directory if it does not exist
-        if (!mediaStorageDir.exists() && !mediaStorageDir.mkdirs()) {
-            Log.d(TAG, "failed to create directory")
-        }
-
-        // Return the file target for the photo based on filename
-        return File(mediaStorageDir.path + File.separator + fileName)
-    }
-
-    //MAKING QUERIES FOR POSTS IN THE SERVER
-    fun queryPosts() {
-        val query: ParseQuery<Post> = ParseQuery.getQuery(Post::class.java)
-        query.include(Post.KEY_USER)
-        query.findInBackground(object: FindCallback<Post> {
-            override fun done(posts: MutableList<Post>?, e: com.parse.ParseException?) {
-                if(e != null){
-                    Log.e(TAG, "Error fetching posts!")
-                }else{
-                    if(posts != null){
-                        for(post in posts){
-                            Log.i(TAG, "Post " + post.getDescription() +", from: " +
-                            post.getUser()?.username)
-                        }
-                    }
+                R.id.action_home ->{
+                    fragmentToShow = HomeFragment()
+                }
+                R.id.action_compose -> {
+                    fragmentToShow = ComposeFragment()
+                }
+                R.id.action_profile -> {
+                    fragmentToShow = ProfileFragment()
                 }
             }
-        })
+
+            if(fragmentToShow != null){
+                fragmentManager.beginTransaction().replace(R.id.flContainer, fragmentToShow).commit()
+            }
+            true
+        }
+
+        // Set default selection
+        findViewById<BottomNavigationView>(R.id.bottom_navigation).selectedItemId = R.id.action_home
+        //queryPosts()
     }
+
+
     companion object{
         const val TAG = "MainActivity"
-}
+    }
 
 }
